3f4e57504834ee8aafd75e3fd19e8265376a47b6
Renato Novaes <renatoalmeida49@gmail.com>
2021-08-05 07:23:51 -0300

    feat(script.js): menu mobile logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-// alert('It works!')
+const buttonMenu = document.querySelector('.button-mobile')
+let imgButton = document.querySelector('.button-mobile img')
+const menu = document.querySelector('.menu')
+
+buttonMenu.addEventListener('click', () => {
+    if (imgButton.getAttribute("src") === './assets/images/icon-close.svg')
+        imgButton.setAttribute("src", './assets/images/icon-hamburger.svg')
+    else
+        imgButton.setAttribute("src", './assets/images/icon-close.svg')
+
+    menu.classList.toggle('active')
+})
